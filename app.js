c6798c553c7b69b966aec2dbda570903fa9908c4
Thuong Ho <thuong.t.ho@gmail.com>
2016-03-28 19:42:24 -0700

    set up mocha in gulp

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  var express = require('express');
+    var express = require('express');
 
-  var app = express();
+    var app = express();
 
-  var port = process.env.PORT || 3000;
+    var port = process.env.PORT || 3000;
 
-  app.get('/', function(req, res) {
-    res.send('Welcome to LunchWith API');
-  });
+    app.get('/', function (req, res) {
+        res.send('Welcome to LunchWith API');
+    });
 
-  app.listen(port, function() {
-    console.log('Running on port: ' + port);
-  });
+    app.listen(port, function () {
+        console.log('Running on port: ' + port);
+    });
 
-  module.exports = app;
+    module.exports = app;
 
-})();
+}());
--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,34 @@
-(function(){
-  'use strict'
+(function () {
+    'use strict';
 
-  var gulp = require('gulp'),
-      nodemon = require('gulp-nodemon');
+    var gulp = require('gulp'),
+        nodemon = require('gulp-nodemon'),
+        jshint = require('gulp-jshint'),
+        gulpMocha = require('gulp-mocha');
 
-
-  gulp.task('default', function() {
-    nodemon({
-      script: 'app.js',
-      ext: 'js',
-      env: {
-        PORT: 8000
-      },
-      ignore: ['./node_modules/**']
-    })
-    .on('restart', function() {
-      console.log('Restarting...');
+    gulp.task('lint', function () {
+        gulp.src('./**/*.js')
+            .pipe(jshint());
     });
-  });
 
-})();
+    gulp.task('test', function () {
+        gulp.src('tests/*.js', {read: false})
+            .pipe(gulpMocha({reporter: 'nyan'}));
+    });
+
+    gulp.task('default', function () {
+        nodemon({
+            script: 'app.js',
+            ext: 'js',
+            env: {
+                PORT: 8000
+            },
+            ignore: ['./node_modules/**'],
+            tasks: ['lint', 'test']
+        })
+            .on('restart', function () {
+                console.log('Restarting...');
+            });
+    });
+
+}());
