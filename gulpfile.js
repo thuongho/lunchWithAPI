d8bced902e955800177b72fb3b2d55767db07b5d
Thuong Ho <thuong.t.ho@gmail.com>
2016-03-28 18:52:40 -0700

    use nodemon and gulp to watch for changes

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+(function(){
+  'use strict'
+
+  var gulp = require('gulp'),
+      nodemon = require('gulp-nodemon');
+
+
+  gulp.task('default', function() {
+    nodemon({
+      script: 'app.js',
+      ext: 'js',
+      env: {
+        PORT: 8000
+      },
+      ignore: ['./node_modules/**']
+    })
+    .on('restart', function() {
+      console.log('Restarting...');
+    });
+  });
+
+})();
